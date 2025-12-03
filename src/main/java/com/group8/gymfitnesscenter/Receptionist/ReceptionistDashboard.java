4205207aa267a02a32defff4c6f914acadf58e84
NusratJahanSumaiya2110096 <2110096@iub.edu.bd>
2025-12-03 21:22:34 +0600

    Sixth

--- src/main/java/com/group8/gymfitnesscenter/Receptionist/ReceptionistDashboard.java
+++ src/main/java/com/group8/gymfitnesscenter/Receptionist/ReceptionistDashboard.java
@@ -1,7 +1,15 @@
 package com.group8.gymfitnesscenter.Receptionist;
 
+import com.group8.gymfitnesscenter.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ReceptionistDashboard
 {
@@ -11,34 +19,115 @@ public class ReceptionistDashboard
     }
 
     @javafx.fxml.FXML
-    public void membershippaymentsOAbutton(ActionEvent actionEvent) {
+    public void membershippaymentsOAbutton(ActionEvent actionEvent) throws IOException {
+
+        Parent root=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MembershipsPayment.fxml"));
+        root = fxmlLoader.load();
+
+
+        Scene scene = new Scene(root) ;
+        Stage stage =(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void classschedulemanagementOAbutton(ActionEvent actionEvent) {
+    public void classschedulemanagementOAbutton(ActionEvent actionEvent) throws IOException {
+        Parent root=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ClassScheduleManagement.fxml"));
+        root = fxmlLoader.load();
+
+
+        Scene scene = new Scene(root) ;
+        Stage stage =(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void inquiryandcomplaintdeskOAbutton(ActionEvent actionEvent) {
+    public void inquiryandcomplaintdeskOAbutton(ActionEvent actionEvent) throws IOException {
+        Parent root=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("InquiryComplaintDesk.fxml"));
+        root = fxmlLoader.load();
+
+
+        Scene scene = new Scene(root) ;
+        Stage stage =(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void memberregistrationOAbutton(ActionEvent actionEvent) {
+    public void memberregistrationOAbutton(ActionEvent actionEvent) throws IOException {
+        Parent root=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MemberRegistration.fxml"));
+        root = fxmlLoader.load();
+
+
+        Scene scene = new Scene(root) ;
+        Stage stage =(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void dailyattendancereportsOAbutton(ActionEvent actionEvent) {
+    public void dailyattendancereportsOAbutton(ActionEvent actionEvent) throws IOException {
+        Parent root=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("DailyAttendanceReports.fxml"));
+        root = fxmlLoader.load();
+
+
+        Scene scene = new Scene(root) ;
+        Stage stage =(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void bookingapprovalcenterOAbutton(ActionEvent actionEvent) {
+    public void bookingapprovalcenterOAbutton(ActionEvent actionEvent) throws IOException {
+        Parent root=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("BookingApprovalCenter.fxml"));
+        root = fxmlLoader.load();
+
+
+        Scene scene = new Scene(root) ;
+        Stage stage =(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void renewalremindersystemOAbutton(ActionEvent actionEvent) {
+    public void renewalremindersystemOAbutton(ActionEvent actionEvent) throws IOException {
+        Parent root=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("RenewalReminderSystem.fxml"));
+        root = fxmlLoader.load();
+
+
+        Scene scene = new Scene(root) ;
+        Stage stage =(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void attendancecheckinoutOAbutton(ActionEvent actionEvent) {
+    public void attendancecheckinoutOAbutton(ActionEvent actionEvent) throws IOException {
+        Parent root=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("AttendanceCheck.fxml"));
+        root = fxmlLoader.load();
+
+
+        Scene scene = new Scene(root) ;
+        Stage stage =(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
     }
 }

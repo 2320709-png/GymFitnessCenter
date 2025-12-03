48cf71934477337ffa3854d9853f12f9a573f478
NusratJahanSumaiya2110096 <2110096@iub.edu.bd>
2025-12-03 21:21:48 +0600

    Fifth

--- src/main/java/com/group8/gymfitnesscenter/Nutritionist/NutritionistDashboard.java
+++ src/main/java/com/group8/gymfitnesscenter/Nutritionist/NutritionistDashboard.java
@@ -1,7 +1,14 @@
 package com.group8.gymfitnesscenter.Nutritionist;
 
+import com.group8.gymfitnesscenter.HelloApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class NutritionistDashboard
 {
@@ -10,35 +17,122 @@ public class NutritionistDashboard
     }
 
     @javafx.fxml.FXML
-    public void allergiesandrestrictionsonactionbutton(ActionEvent actionEvent) {
+    public void allergiesandrestrictionsonactionbutton(ActionEvent actionEvent) throws IOException {
+
+        Parent root=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("HealthAlerts.fxml"));
+        root = fxmlLoader.load();
+
+
+        Scene scene = new Scene(root) ;
+        Stage stage =(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void consultwithtraineronactionbutton(ActionEvent actionEvent) {
+    public void consultwithtraineronactionbutton(ActionEvent actionEvent) throws IOException {
+
+        Parent root=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("TrainerConsultration.fxml"));
+        root = fxmlLoader.load();
+
+
+        Scene scene = new Scene(root) ;
+        Stage stage =(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
 
     }
 
     @javafx.fxml.FXML
-    public void groupnutritionworkshopsonactionbutton(ActionEvent actionEvent) {
+    public void groupnutritionworkshopsonactionbutton(ActionEvent actionEvent) throws IOException {
+
+        Parent root=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("NutritionWorkshop.fxml"));
+        root = fxmlLoader.load();
+
+
+        Scene scene = new Scene(root) ;
+        Stage stage =(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void trackclienteatinghabitsonactionbutton(ActionEvent actionEvent) {
+    public void trackclienteatinghabitsonactionbutton(ActionEvent actionEvent) throws IOException {
+
+        Parent root=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ClientEatingHabit.fxml"));
+        root = fxmlLoader.load();
+
+
+        Scene scene = new Scene(root) ;
+        Stage stage =(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void dietplansonactionbutton(ActionEvent actionEvent) {
+    public void dietplansonactionbutton(ActionEvent actionEvent) throws IOException {
+
+        Parent root=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("PersonalizedDietPlan.fxml"));
+        root = fxmlLoader.load();
+
+
+        Scene scene = new Scene(root) ;
+        Stage stage =(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void adjustdietsonactionbutton(ActionEvent actionEvent) {
+    public void adjustdietsonactionbutton(ActionEvent actionEvent) throws IOException {
+        Parent root=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("AdjustDietPlan.fxml"));
+        root = fxmlLoader.load();
+
+
+        Scene scene = new Scene(root) ;
+        Stage stage =(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void dietremindersonactionbutton(ActionEvent actionEvent) {
+    public void dietremindersonactionbutton(ActionEvent actionEvent) throws IOException {
+
+        Parent root=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("DietReminders.fxml"));
+        root = fxmlLoader.load();
+
+
+        Scene scene = new Scene(root) ;
+        Stage stage =(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void updatenutritionlibraryonactionbutton(ActionEvent actionEvent) {
+    public void updatenutritionlibraryonactionbutton(ActionEvent actionEvent) throws IOException {
+
+        Parent root=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("NutritionLibrary.fxml"));
+        root = fxmlLoader.load();
+
+
+        Scene scene = new Scene(root) ;
+        Stage stage =(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
     }
 }

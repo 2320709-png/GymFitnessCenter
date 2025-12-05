cd8b6de7ae18597086af52be209a5bdbef7a54ae
2320709-png <2320709@iub.edu.bd>
2025-12-05 19:05:50 +0600

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,17 @@ module com.group8.gymfitnesscenter {
     requires javafx.fxml;
 
 
+
+
+
     opens com.group8.gymfitnesscenter to javafx.fxml;
     exports com.group8.gymfitnesscenter;
+    exports com.group8.gymfitnesscenter.Lamia;
+    opens com.group8.gymfitnesscenter.Lamia to javafx.fxml;
+    exports com.group8.gymfitnesscenter.Lamia.Member;
+    opens com.group8.gymfitnesscenter.Lamia.Member to javafx.fxml;
+    exports com.group8.gymfitnesscenter.Lamia.Trainer;
+    opens com.group8.gymfitnesscenter.Lamia.Trainer to javafx.fxml;
+    exports com.group8.gymfitnesscenter.salesManager;
+    opens com.group8.gymfitnesscenter.salesManager to javafx.fxml;
 }
